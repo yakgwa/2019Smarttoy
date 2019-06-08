a7bbd7208c7aa5efd06e2c4257f9b6a0f09f5120
hyun-woo <benedict0913@naver.com>
2019-06-08 09:26:20 +0900

    edited 2019Smarttoy

---
+++ Comm/LEDSerial.ino
@@ -0,0 +1,22 @@
+void setup() {
+  Serial.begin(9600);    // opens serial port, sets data rate to 9600 bps
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    int incomingByte = Serial.read();
+    if(incomingByte == '1')
+    {
+      digitalWrite(LED_BUILTIN, HIGH);//(HIGH is the voltage level)
+      Serial.print("On");
+    }
+    else if(incomingByte == '0')
+    {
+      digitalWrite(LED_BUILTIN, LOW); //making the voltage LOW
+      Serial.print("Off");
+    }
+  }
+  
+}
+© 2019 GitHub, Inc.
